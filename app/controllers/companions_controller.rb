c1e99dd4eb17a916461c2c66f49bb8851868a95b
Katja Loeffler <katja.loeffler88@gmail.com>
2023-03-02 21:18:55 +0100

    delete companion button

--- app/controllers/companions_controller.rb
+++ app/controllers/companions_controller.rb
@@ -32,10 +32,10 @@ class CompanionsController < ApplicationController
   #   redirect_to companion_path(@companion)
   # end
 
-  # def destroy
-  #   @companion.destroy
-  #   redirect_to companions_path, status: :see_other
-  # end
+  def destroy
+    @companion.destroy
+    redirect_to companions_path, status: :see_other
+  end
 
   private
 
